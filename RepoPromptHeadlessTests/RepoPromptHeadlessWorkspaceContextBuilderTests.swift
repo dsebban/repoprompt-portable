f905888ba8a52685dfe026c568afa9476e69bdeb
dsebban <daniel.sebban@gmail.com>
2026-07-23 20:55:52 +0300

    test: make symlink assertion portable

--- RepoPromptHeadlessTests/RepoPromptHeadlessWorkspaceContextBuilderTests.swift
+++ RepoPromptHeadlessTests/RepoPromptHeadlessWorkspaceContextBuilderTests.swift
@@ -110,7 +110,7 @@ final class RepoPromptHeadlessWorkspaceContextBuilderTests: XCTestCase {
 		)
 
 		XCTAssertTrue(context.entries.isEmpty)
-		XCTAssertTrue(context.omissions.contains { $0.path == "selected.txt" && $0.reason == .symlinkEscape })
+		XCTAssertTrue(context.omissions.contains { $0.reason == .symlinkEscape })
 		XCTAssertFalse(context.content.contains("OUTSIDE_SECRET_SENTINEL"))
 	}
 
